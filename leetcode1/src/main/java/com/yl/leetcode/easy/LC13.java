7600469610735a8342c26823b047e0669591c9fe
yinlei <yinlei1993@gmail.com>
2022-08-18 00:46:21 +0800

    add 13

---
+++ leetcode1/src/main/java/com/yl/leetcode/easy/LC13.java
@@ -0,0 +1,62 @@
+package com.yl.leetcode.easy;
+
+/**
+ * @author Lei
+ * @version 1.0
+ * @date 2022/8/17 - 08 - 17 - 23:18
+ * @Description Roman to Integer
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * <p>
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ * <p>
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ * <p>
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ */
+public class LC13 {
+
+    public int romanToInt(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int current = mapping(s.charAt(i));
+            if (i < s.length() - 1 && current < mapping(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+        return result;
+    }
+
+    public int mapping(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+}
