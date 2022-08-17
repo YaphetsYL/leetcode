780ceab0a77c0bb21178987fb2743881e9b606f1
yinlei <yinlei1993@gmail.com>
2022-08-17 22:54:32 +0800

    387 final

--- leetcode1/src/main/java/com/yl/leetcode/easy/LC387.java
+++ leetcode1/src/main/java/com/yl/leetcode/easy/LC387.java
@@ -9,11 +9,46 @@ package com.yl.leetcode.easy;
 public class LC387 {
 
     public int firstUniqChar(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if(s.indexOf(s.charAt(i))==s.lastIndexOf(s.charAt(i))){
+        int[] count = new int[26];
+        int n = s.length();
+        // build char count bucket : <charIndex, count>
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i) - 'a';
+            count[index]++;
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i) - 'a';
+            if (count[index] == 1) {
                 return i;
             }
         }
         return -1;
+
     }
+    /*public int firstUniqChar(String s) {
+        int index[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int character = s.charAt(i) - 'a';
+            index[character]++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            int character = s.charAt(i) - 'a';
+            if (index[character] == 1) {
+                return i;
+            }
+
+        }
+        return -1;
+    }*/
+
+    /*public int firstUniqChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }*/
 }
