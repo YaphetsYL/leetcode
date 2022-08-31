1afd3cc6cd4cf6225dc736c7e74a4a99d01ca676
yinlei <yinlei1993@gmail.com>
2022-08-31 23:49:33 +0800

    add 48

---
+++ leetcode1/src/main/java/com/yl/leetcode/medium/aug22/LC48.java
@@ -0,0 +1,59 @@
+package com.yl.leetcode.medium.aug22;
+
+/**
+ * @author Lei
+ * @version 1.0
+ * @date 2022/8/31 - 08 - 31 - 0:28
+ * @Description Rotate Image
+ * <p>
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ * <p>
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ * DO NOT allocate another 2D matrix and do the rotation.
+ */
+public class LC48 {
+
+    public void rotate(int[][] matrix) {
+        //transpose
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //reflect
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+
+
+
+
+
+
+    /*public void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i =0 ; i<matrix.length; i++){
+            for(int j = 0; j<matrix.length/2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }*/
+}
